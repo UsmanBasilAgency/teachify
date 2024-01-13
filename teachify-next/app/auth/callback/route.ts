65be2a44a1116390df2ada4c47af4096005262b4
Usman Zaheer <usmanozaheer@gmail.com>
2024-01-12 20:03:11 -0500

    t

--- teachify-next/app/auth/callback/route.ts
+++ teachify-next/app/auth/callback/route.ts
@@ -3,18 +3,20 @@ import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
 export async function GET(request: Request) {
-    // The `/auth/callback` route is required for the server-side auth flow implemented
-    // by the Auth Helpers package. It exchanges an auth code for the user's session.
-    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
-    const requestUrl = new URL(request.url)
-    const code = requestUrl.searchParams.get('code')
-
+    const { searchParams, origin } = new URL(request.url)
+    const code = searchParams.get('code')
+    // if "next" is in param, use it as the redirect URL
+    const next = searchParams.get('next') ?? '/'
+  
     if (code) {
-        const cookieStore = cookies()
-        const supabase = createClient(cookieStore)
-        await supabase.auth.exchangeCodeForSession(code)
+      const cookieStore = cookies()
+      const supabase = createClient(cookieStore);
+      const { error } = await supabase.auth.exchangeCodeForSession(code)
+      if (!error) {
+        return NextResponse.redirect(`${origin}${next}`)
+      }
     }
-
-    // URL to redirect to after sign in process completes
-    return NextResponse.redirect(requestUrl.origin)
-}
+  
+    // return the user to an error page with instructions
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  }
--- teachify-next/app/login/page.tsx
+++ teachify-next/app/login/page.tsx
@@ -50,7 +50,18 @@ export default function Login() {
     const signUpWithGoogle = async () => {
         "use server";
 
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+        const cookieStore = cookies();
+        const supabase = createClient(cookieStore);
+
+        const { error } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+        });
+
+        if (error) {
+            return redirect(`/login?message=${error.message}`);
+        }
+
+        return redirect("/");
     };
 
     return (
@@ -129,36 +140,35 @@ export default function Login() {
 
 
                             <button
-                                formAction={signUpWithGoogle}
-                                className="w-full text-black bg-dark-custom-blue hover:bg-custom-blue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center text-center inline-flex items-center justify-between"
-                            >
-                                <svg
-                                    className="mr-2 -ml-1 w-4 h-4"
-                                    aria-hidden="true"
-                                    focusable="false"
-                                    data-prefix="fab"
-                                    data-icon="google"
-                                    role="img"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 488 512"
-                                >
-                                    <path
-                                        fill="currentColor"
-                                        d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                                    ></path>
-                                </svg>
-                                Sign in with Google
-                                <div></div>
-                            </button>
-
-
-                            <button
                                 formAction={signUp}
                                 className="w-full text-black bg-dark-custom-blue hover:bg-custom-blue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                             >
                                 Create Account with Email
                             </button>
                         </form>
+
+                        <button
+                            onClick={signUpWithGoogle}
+                            className="w-full text-black bg-dark-custom-blue hover:bg-custom-blue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center text-center inline-flex items-center justify-between"
+                        >
+                            <svg
+                                className="mr-2 -ml-1 w-4 h-4"
+                                aria-hidden="true"
+                                focusable="false"
+                                data-prefix="fab"
+                                data-icon="google"
+                                role="img"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 488 512"
+                            >
+                                <path
+                                    fill="currentColor"
+                                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                                ></path>
+                            </svg>
+                            Sign in with Google
+                            <div></div>
+                        </button>
                     </div>
                 </div>
             </div>
