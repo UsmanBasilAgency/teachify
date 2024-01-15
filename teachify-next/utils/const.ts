7e3fd00f9ac3498a9e518d51e44739660817d89f
Usman Zaheer <usmanozaheer@gmail.com>
2024-01-15 17:02:19 -0500

    add changes

--- teachify-next/utils/const.ts
+++ teachify-next/utils/const.ts
@@ -5,7 +5,12 @@ enum Tables {
 }
 
 enum Models {
-    mistral = "mistralai/mistral-7b-instruct"
+    mistral = "mistralai/mistral-7b-instruct",
+    huggingFace = "huggingfaceh4/zephyr-7b-beta",
+    openChat = "openchat/openchat-7b",
+    capybara = "nousresearch/nous-capybara-7b",
+    mythoMist = "gryphe/mythomist-7b",
+    gpt3 = "openai/gpt-3.5-turbo"
 }
 
 export { DARK_MODE_CLASS_NAME, Tables, Models };
