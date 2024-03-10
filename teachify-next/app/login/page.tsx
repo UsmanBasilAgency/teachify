ca17e6034d7249e1f133e5db1467f7ec2d9b5bd6
Usman Zaheer <usmanozaheer@gmail.com>
2024-03-09 19:17:37 -0500

    test

--- teachify-next/app/login/page.tsx
+++ teachify-next/app/login/page.tsx
@@ -63,6 +63,8 @@ export default function Login() {
             password
         });
 
+        console.log('test')
+
         if (error) {
             setError(error.toString());
         } else {
